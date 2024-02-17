2dce6e2fc8f0b0857409b6ea90eedc3ddfb7e37a
web-dev-one <solutionsllcwebmasters@gmail.com>
2024-02-16 21:48:19 -0700

    update

--- FrontEnd/Pet-Safe-Kennels/logs.tf
+++
@@ -1,4 +0,0 @@
-resource "aws_cloudwatch_log_group" "kennels_app" {
-  name              = "/ecs/kennels"
-  retention_in_days = 1
-}
--- FrontEnd/Pet-Safe-Kennels/s3.tf
+++
@@ -1,7 +0,0 @@
-resource "aws_s3_bucket" "codepipeline_artifacts_next" {
-  bucket = "${var.region}-${var.name}-artifacts-bucket"
-
-  tags = {
-    Name = "artifacts-bucket"
-  }
-}
